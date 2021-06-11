f397fd4542e2bc9a9d292df41a8ff6c57c420c6a
Bruce Wells <brucekwells@gmail.com>
2021-06-11 14:29:56 -0400

    Helper functions to get format types and file extensions

--- src/Stimulsoft/ExportFormat.php
+++ src/Stimulsoft/ExportFormat.php
@@ -3,7 +3,7 @@
 namespace Stimulsoft;
 
 class ExportFormat
-{
+	{
 	const Pdf = 1;
 
 	const Text = 11;
@@ -27,4 +27,36 @@ class ExportFormat
 	const Html5 = 36;
 
 	const Document = 1000;
-}
+
+	/**
+	 * @return array of valid format numbers, indexed by format name
+	 */
+	public static function getFormatNumbers()
+		{
+		$reflection = new \ReflectionClass(__CLASS__);
+
+		return $reflection->getConstants();
+		}
+
+	/**
+	 * @return array of file extensions, indexed by format name
+	 */
+	public static function getFormatExtensions()
+		{
+		return [
+			'Pdf' => 'pdf',
+			'Text' => 'txt',
+			'Excel2007' => 'xlsx',
+			'Word2007' => 'docx',
+			'Csv' => 'csv',
+			'ImageSvg' => 'svg',
+			'Html' => 'html',
+			'Ods' => 'ods',
+			'Odt' => 'odt',
+			'Ppt2007' => 'pptx',
+			'Html5' => 'html',
+			'Document' => 'doc'
+			];
+		}
+
+	}
