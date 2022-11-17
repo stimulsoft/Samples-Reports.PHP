863adf8bdc1b2cec7699d95338609c53c0877236
VladimirHot <hotchenko@gmail.com>
2022-11-17 11:44:34 +0300

    add sample data

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
     <li><a href="Exporting a Report from Code.php">Exporting a Report from Code</a></li>
     <li><a href="How to activate the Product.php">How to activate the Product</a></li>
     <li><a href="Saving a Report template on the Server-side.php">Saving a Report template on the Server-side</a></li>
+    <!--<li><a href="Selective loading of Scripts for Project minimization.php">Selective loading of Scripts for Project minimization</a></li>-->
     <li><a href="Sending a Report by Email.php">Sending a Report by Email</a></li>
     <li><a href="Sending an Exported report to the Server-side.php">Sending an Exported report to the Server-side</a></li>
     <li><a href="Setting Report variables on the Server-side.php">Setting Report variables on the Server-side</a></li>
