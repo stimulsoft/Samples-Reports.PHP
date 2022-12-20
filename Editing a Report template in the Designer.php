dba41f6d7031dac07fa57aaf51544402460772ac
Tim Kubekin <68532402+tsimafeikubekin@users.noreply.github.com>
2022-12-20 08:51:32 +0100

    Renaming

--- Editing a Report template in the Designer.php
+++ Editing a Report template in the Designer.php
@@ -15,7 +15,7 @@ use Stimulsoft\StiJavaScript;
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <title>Editing a Report template in the Designer</title>
+    <title>Editing a Report Template in the Designer</title>
     <style>
         html, body {
             font-family: sans-serif;
