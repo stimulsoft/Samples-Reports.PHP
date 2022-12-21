b99f62df992d31f9d5cb36215a041226a02495b1
Tim Kubekin <68532402+tsimafeikubekin@users.noreply.github.com>
2022-12-21 11:36:53 +0100

    Some fixes

--- index.php
+++ index.php
@@ -15,19 +15,19 @@
 <hr/>
 <ul>
     <li><a href="Changing the Viewer Theme.php">Changing the Viewer Theme</a></li>
-    <li><a href="Editing a Report template in the Designer.php">Editing a Report template in the Designer</a></li>
-    <li><a href="Editing a Report template in the Designer using JavaScript.php">Editing a Report template in the Designer using JavaScript</a></li>
+    <li><a href="Editing a Report Template in the Designer.php">Editing a Report Template in the Designer</a></li>
+    <li><a href="Editing a Report Template in the Designer using JavaScript.php">Editing a Report Template in the Designer using JavaScript</a></li>
     <li><a href="Exporting a Report from Code.php">Exporting a Report from Code</a></li>
-    <li><a href="How to activate the Product.php">How to activate the Product</a></li>
+    <li><a href="How to Activate the Product.php">How to Activate the Product</a></li>
     <li><a href="Rendering a Report from Code.php">Rendering a Report from Code</a></li>
-    <li><a href="Saving a Report template on the Server-side.php">Saving a Report template on the Server-side</a></li>
-    <!--<li><a href="Selective loading of Scripts for Project minimization.php">Selective loading of Scripts for Project minimization</a></li>-->
+    <li><a href="Saving a Report Template on the Server-Side.php">Saving a Report Template on the Server-Side</a></li>
+    <!--<li><a href="Loading Scripts in Part to Minify Project.php">Loading Scripts in Part to Minify Project</a></li>-->
     <li><a href="Sending a Report by Email.php">Sending a Report by Email</a></li>
-    <li><a href="Sending an Exported report to the Server-side.php">Sending an Exported report to the Server-side</a></li>
-    <li><a href="Setting Report variables on the Server-side.php">Setting Report variables on the Server-side</a></li>
+    <li><a href="Sending an Exported report to the Server-side.php">Sending an Exported Report to the Server-Side</a></li>
+    <li><a href="Setting Report variables on the Server-side.php">Setting Report Variables on the Server-Side</a></li>
     <li><a href="Showing a Report in the Viewer.php">Showing a Report in the Viewer</a></li>
     <li><a href="Showing a Report in the Viewer using JavaScript.php">Showing a Report in the Viewer using JavaScript</a></li>
-    <li><a href="Using SQL Data Sources.php">Using SQL Data Sources</a></li>
+    <li><a href="Using a SQL Data Sources.php">Using a SQL Data Sources</a></li>
     <li><a href="Using Parameters in SQL Query.php">Using Parameters in SQL Query</a></li>
 </ul>
 
